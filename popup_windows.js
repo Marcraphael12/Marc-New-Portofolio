7b43aff7b225cb54ea911933de68cf7644036e11
Marc Raphael <marcraphael979@gmail.com>
2021-08-24 09:26:13 +0100

    Add the opening and closing functions

---
+++ popup_windows.js
@@ -0,0 +1,36 @@
+/* See project's button */
+const prjt = document.querySelectorAll('.see-project');
+
+/** ********* The card element ******** */
+const card = document.createElement('div');
+card.setAttribute('style', '\
+height: 90%;\
+width: 90%;\
+background: white;\
+display: flex;\
+margin: 0 auto;\
+padding: 0 10px;\
+align-items: center;\
+flex-direction: column;');
+
+/** ********* The modal element ******** */
+const modal = document.createElement('aside'); // Consider modal as an aside element
+modal.setAttribute('style', '\
+height: 100%;\
+width: 100vw;\
+background: #191765;\
+position: fixed;\
+top: 0;\
+left: 0;\
+display: flex;\
+align-items: center;\
+overflow: scroll;');
+modal.appendChild(card);
+
+/** ***** Opening and closing functions part ******** */
+function openProject() { // open the modal
+  document.body.appendChild(modal);
+}
+for (let i = 0; i < prjt.length-1; i++) {
+	prjt[i].addEventListener('click', openProject)
+}
