da960c735da145b48e72e79483de342b24a17eb0
Marc Raphael <marcraphael979@gmail.com>
2021-09-06 18:05:20 +0100

    Start pop up window

--- popup_windows.js
+++ popup_windows.js
@@ -1,2 +1,10 @@
 /* eslint-disable no-multi-str */
 /* eslint-disable no-plusplus */
+const aside = document.createElement('aside');
+const openProject = document.querySelectorAll('.see-project');
+
+for (var i = 0; i < openProject.length; i++) {
+	openProject[i].addEventListener('click', () => {
+		sct2.appendChild(aside);
+	})
+}
