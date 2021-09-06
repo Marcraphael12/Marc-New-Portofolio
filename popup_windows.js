818bec85d766b9de16f830915ae62474d590d3fe
Marc Raphael <marcraphael979@gmail.com>
2021-09-06 19:23:09 +0100

    Create the closing project button
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- popup_windows.js
+++ popup_windows.js
@@ -9,9 +9,16 @@ const closeProject = document.createElement('button');
 
 /* Basic script */
 
+closeProject.innerHTML = '&times;';
+closeProject.classList.add('close-project')
 
 for (var i = 0; i < openProject.length; i++) {
 	openProject[i].addEventListener('click', () => {
+		aside.appendChild(closeProject);
 		sct2.appendChild(aside);
 	});
 }
+
+closeProject.addEventListener('click', () => {
+	sct2.removeChild(aside);
+});
