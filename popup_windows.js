fb04bddcc537b886249d2e4ef281709222c80977
Marc Raphael <marcraphael979@gmail.com>
2021-09-06 18:48:25 +0100

    Declare the const for the closing button
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- popup_windows.js
+++ popup_windows.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-multi-str */
 /* eslint-disable no-plusplus */
+
+/* Const and var declaration */
+const container = document.createElement('div');
 const aside = document.createElement('aside');
 const openProject = document.querySelectorAll('.see-project');
+const closeProject = document.createElement('button');
+
+/* Basic script */
+
 
 for (var i = 0; i < openProject.length; i++) {
 	openProject[i].addEventListener('click', () => {
 		sct2.appendChild(aside);
-	})
+	});
 }
