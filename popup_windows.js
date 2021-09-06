8ca3fdb2db785fd37856e3b06cf5a4bdc9b3ddb9
Marc Raphael <marcraphael979@gmail.com>
2021-09-06 21:29:42 +0100

    Create objects for images and text
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- popup_windows.js
+++ popup_windows.js
@@ -7,6 +7,30 @@ const aside = document.createElement('aside');
 const openProject = document.querySelectorAll('.see-project');
 const closeProject = document.createElement('button');
 
+const image = {
+  source: './images/photos/project_img.png'
+}
+
+const text = {
+  txtDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the\
+  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and\
+  scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry\
+  . Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum\
+  is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard\
+  dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+
+  txtMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.\
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown\
+  printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy\
+  text of the printing and typesetting  ever since the 1500s, when an unknown printer took a\
+  galley of type veris lapoa todoe.",
+
+  html: 'html',
+  css: 'css',
+  ror: 'Ruby on Rails',
+  github: 'Github'
+}
+
 /* Basic script */
 
 closeProject.innerHTML = '&times;';
@@ -19,16 +43,21 @@ title.classList.add('popup-title');
 title.appendChild(h1);
 title.appendChild(closeProject);
 
+const popupImage = document.createElement('img');
+popupImage.setAttribute('src', image.source);
+popupImage.classList.add('popup-image');
+
 container.classList.add('project-card');
 container.appendChild(title)
+container.appendChild(popupImage)
 
 for (var i = 0; i < openProject.length; i++) {
-	openProject[i].addEventListener('click', () => {
-		aside.appendChild(container);
-		sct2.appendChild(aside);
-	});
+  openProject[i].addEventListener('click', () => {
+  aside.appendChild(container);
+  sct2.appendChild(aside);
+  });
 }
 
 closeProject.addEventListener('click', () => {
-	sct2.removeChild(aside);
+  sct2.removeChild(aside);
 });
