e220991e9363f69dde3772f6e7d434c26128c6d6
Marc Raphael <marcraphael979@gmail.com>
2021-09-08 14:32:07 +0100

    Add custom message error for email
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- form_data.js
+++ form_data.js
@@ -2,3 +2,13 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable quotes */
 /* eslint-disable prefer-destructuring */
+
+const email = document.querySelector('.email');
+
+email.addEventListener("keyup", function (event) {
+  if(email.validity.typeMismatch) {
+    email.setCustomValidity("Hey dear, Sorry but i'm waiting an email here!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
