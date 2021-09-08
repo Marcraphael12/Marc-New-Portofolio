2fee9d86966d0ca766f26fe56ebf434b8f38e56a
Marc Raphael <marcraphael979@gmail.com>
2021-09-08 15:36:30 +0100

    Adjust code
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- form_data.js
+++ form_data.js
@@ -7,25 +7,25 @@ const email = document.querySelector('.email');
 const inputs = document.querySelectorAll('.input');
 const lowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str); // functio to test if the email value is lower Case only
 
-email.addEventListener("keyup", function (event) {
-  if(email.validity.typeMismatch) {
+email.addEventListener("keyup", () => {
+  if (email.validity.typeMismatch) {
     email.setCustomValidity("Hey dear, Sorry but i'm waiting an email here!");
   } else {
     email.setCustomValidity("");
   }
 
   if (!lowerCase(email.value)) {
-  email.setCustomValidity("Hey dear, use lower case only please!")
+    email.setCustomValidity("Hey dear, use lower case only please!");
   }
 });
 
 for (let i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("keyup", function (event) {
-  if (inputs[i].validity.valid) {
-  inputs[i].style.border = '2px solid green';
-  }
-  else {
-  inputs[i].style.border = '2px solid red';
-  }
+  inputs[i].addEventListener("keyup", () => {
+    if (inputs[i].validity.valid) {
+      inputs[i].style.border = '2px solid green';
+    }
+    else {
+      inputs[i].style.border = '2px solid red';
+    }
   });
 }
