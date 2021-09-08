c5d702ff437404e300d4242a68324dd82ef320e1
Marc Raphael <marcraphael979@gmail.com>
2021-09-08 15:00:32 +0100

    Create validation for all fields
    Co-Authored-By: Darren Odi <darrenodi@users.noreply.github.com>

--- form_data.js
+++ form_data.js
@@ -4,6 +4,7 @@
 /* eslint-disable prefer-destructuring */
 
 const email = document.querySelector('.email');
+const inputs = document.querySelectorAll('.input');
 
 email.addEventListener("keyup", function (event) {
   if(email.validity.typeMismatch) {
@@ -12,3 +13,14 @@ email.addEventListener("keyup", function (event) {
     email.setCustomValidity("");
   }
 });
+
+for (let i = 0; i < inputs.length; i++) {
+	inputs[i].addEventListener("keyup", function (event) {
+		if (inputs[i].validity.valid) {
+			inputs[i].style.border = '2px solid green';
+		}
+		else {
+			inputs[i].style.border = '2px solid red';
+		}
+	});
+}
